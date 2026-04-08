dc19470c9d21286fd15144cf3402eb31a0781112
7x <info@se7enx.com>
2026-04-07 19:41:55 -0700

    feat: rename LegacyEmbedScriptCommand to exponential:legacy:script, keep ezpublish:legacy:script as deprecated alias (v4/4.x)
    The LegacyEmbedScriptCommand is renamed from ezpublish:legacy:script to
    exponential:legacy:script. The old name is kept as a deprecated alias via setAliases().

--- bundle/Command/LegacyEmbedScriptCommand.php
+++ bundle/Command/LegacyEmbedScriptCommand.php
@@ -36,7 +36,8 @@ class LegacyEmbedScriptCommand extends Command
     protected function configure()
     {
         $this
-            ->setName('ezpublish:legacy:script')
+            ->setName('exponential:legacy:script')
+            ->setAliases(['ezpublish:legacy:script'])
             ->addArgument('script', InputArgument::REQUIRED, 'Path to legacy script you want to run. Path must be relative to the legacy root')
             ->addOption('legacy-help', null, InputOption::VALUE_NONE, 'Use this option if you want to display help for the legacy script')
             ->setDescription('Runs an eZ Publish legacy script.')
